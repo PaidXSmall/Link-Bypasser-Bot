a2a18e7e4303085a29c614bedc43e2a7ae6ec41d
PaidXSmall <128967985+PaidXSmall@users.noreply.github.com>
2023-06-07 16:44:47 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1505,16 +1505,18 @@ def tiny(url):
 
 def dalink(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
-    DOMAIN = "https://www.tamilhit.tech/"
+    DOMAIN = "https://dalink.in"
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    resp = client.get(final_url)
+    ref = "https://www.tamilhit.tech/"
+    h = {"referer": ref}
+    resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
     inputs = soup.find_all("input")
     data = {input.get("name"): input.get("value") for input in inputs}
     h = {"x-requested-with": "XMLHttpRequest"}
-    time.sleep(3)
+    time.sleep(7)
     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
     try:
         return r.json()["url"]
